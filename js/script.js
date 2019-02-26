1b549fac2ccb5f3ebd386ed5aa05384527eda7fc
Владимир Ливинский <xDamneDx@Gmail.com>
2019-02-26 20:48:24 +0200

    Закончил проект.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var bookingForm = document.querySelector(".booking-form-wrapper");
 var bookingBtn = document.querySelector(".booking-btn");
+var inputFirst = bookingForm.querySelector("#check-in");
+var inputSecond = bookingForm.querySelector("#check-out");
+
+bookingForm.classList.remove("booking-form-show");
 
 bookingBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -13,3 +17,12 @@ bookingBtn.addEventListener('click', function (evt) {
     bookingForm.classList.add("booking-form-show");
   }
 });
+
+bookingForm.addEventListener('submit', function(evt) {
+  bookingForm.classList.remove("on-error");
+  bookingForm.offsetWidth = bookingForm.offsetWidth;
+  if (inputFirst.value === "" || inputSecond.value === "") {
+    evt.preventDefault();
+    bookingForm.classList.add("on-error");
+  }
+})
